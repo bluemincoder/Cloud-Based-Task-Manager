fa2fc7c2ae8bfe55c38e2743f843b895614c92c6
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-01-30 12:46:54 +0530

    Created Task Page

--- client/src/components/task/TaskDialog.jsx
+++ client/src/components/task/TaskDialog.jsx
@@ -86,11 +86,11 @@ const TaskDialog = ({ task }) => {
                     <button
                       onClick={() => deleteClicks()}
                       className={`${
-                        active ? "bg-blue-500 text-white" : "text-red-900"
+                        active ? "bg-blue-500 text-white" : "text-red-500"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       <RiDeleteBin6Line
-                        className='mr-2 h-5 w-5 text-red-400'
+                        className='mr-2 h-5 w-5 text-red-500'
                         aria-hidden='true'
                       />
                       Delete
