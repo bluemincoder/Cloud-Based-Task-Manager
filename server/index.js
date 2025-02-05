fcc25433e8aa01dc80768b4beada4e31fe6055d3
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-02-05 12:44:42 +0530

    created controllers and routes

--- server/index.js
+++ server/index.js
@@ -1,43 +1,37 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 import morgan from "morgan";
-import dbconnection from "./utils/index.js";
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddleware.js";
-
-// const routes = "";
+import routes from "./routes/index.js";
+import { dbConnection } from "./utils/index.js";
 
 dotenv.config();
 
-dbconnection();
+dbConnection();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        methods: ["POST", "PUT", "GET", "DELETE"],
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
 app.use(morgan("dev"));
-
-// app.use("/api", routes);
+app.use("/api", routes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,15 +1,26 @@
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
-const dbconnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("DB connected");
-    } catch (error) {
-        console.log("DB connection error", error);
-    }
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("DB connected");
+  } catch (error) {
+    console.log("DB Error: " + error);
+  }
 };
 
-export default dbconnection;
+export const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // Change sameSite from strict to none when you deploy your app
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict", //prevent CSRF attack
+    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+  });
+};
