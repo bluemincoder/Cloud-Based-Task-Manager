f0f976e1fc82ce0e6d2693b80e8adfcd00b387ef
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-02-14 18:01:36 +0530

    Updated TaskDetails

--- client/src/pages/TaskDetails.jsx
+++ client/src/pages/TaskDetails.jsx
@@ -19,6 +19,10 @@ import Tabs from "../components/Tabs";
 import { PRIOTITYSTYELS, TASK_TYPE, getInitials } from "../utils";
 import Loading from "../components/Loader";
 import Button from "../components/Button";
+import {
+    useGetSingleTaskQuery,
+    usePostTaskActivityMutation,
+} from "../redux/slices/api/taskApiSlice";
 
 const assets = [
     "https://images.pexels.com/photos/2418664/pexels-photo-2418664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -89,8 +93,17 @@ const act_types = [
 const TaskDetails = () => {
     const { id } = useParams();
 
+    const { data, isLoading, refetch } = useGetSingleTaskQuery(id);
     const [selected, setSelected] = useState(0);
-    const task = tasks[3];
+    const task = data?.task;
+
+    if (isLoading) {
+        return (
+            <div className="py-10">
+                <Loading />
+            </div>
+        );
+    }
 
     return (
         <div className="w-full flex flex-col gap-3 mb-4 overflow-y-hidden">
@@ -126,7 +139,7 @@ const TaskDetails = () => {
                                         <div
                                             className={clsx(
                                                 "w-4 h-4 rounded-full",
-                                                TASK_TYPE[task.stage]
+                                                TASK_TYPE[task?.stage]
                                             )}
                                         />
                                         <span className="text-black uppercase">
@@ -134,7 +147,6 @@ const TaskDetails = () => {
                                         </span>
                                     </div>
                                 </div>
-
                                 <p className="text-gray-500">
                                     Created At:{" "}
                                     {new Date(task?.date).toDateString()}
@@ -249,7 +261,11 @@ const TaskDetails = () => {
                     </>
                 ) : (
                     <>
-                        <Activities activity={task?.activities} id={id} />
+                        <Activities
+                            activity={data?.task?.activities}
+                            id={id}
+                            refetch={refetch}
+                        />
                     </>
                 )}
             </Tabs>
@@ -257,12 +273,31 @@ const TaskDetails = () => {
     );
 };
 
-const Activities = ({ activity, id }) => {
+const Activities = ({ activity, id, refetch }) => {
     const [selected, setSelected] = useState(act_types[0]);
     const [text, setText] = useState("");
-    const isLoading = false;
 
-    const handleSubmit = async () => {};
+    const [postTaskActivity, { isLoading }] = usePostTaskActivityMutation();
+
+    const handleSubmit = async () => {
+        try {
+            const activityData = {
+                type: selected?.toLowerCase(),
+                activity: text,
+            };
+            const result = await postTaskActivity({
+                data: activityData,
+                id,
+            }).unwrap();
+            console.log();
+            setText("");
+            toast.success(result?.message);
+            refetch();
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.data?.message || error.error);
+        }
+    };
 
     const Card = ({ item }) => {
         return (
@@ -277,7 +312,7 @@ const Activities = ({ activity, id }) => {
                 </div>
 
                 <div className="flex flex-col gap-y-1 mb-8">
-                    <p className="font-semibold">{item?.by?.name} </p>
+                    <p className="font-semibold">{item?.by?.name}</p>
                     <div className="text-gray-500 space-y-2">
                         <span className="capitalize">{item?.type}</span>
                         <span> </span>
@@ -303,7 +338,7 @@ const Activities = ({ activity, id }) => {
                         <Card
                             key={index}
                             item={el}
-                            isConnected={index < activity.length - 1}
+                            isConnected={index < activity?.length - 1}
                         />
                     ))}
                 </div>
