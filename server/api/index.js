0d8ceb39df3b4784a0e2091cc5efa16cb86a6cc8
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-02-19 16:02:55 +0530

    Fixed API route issue for Vercel cors fixed

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,15 @@
 import app from "../index.js";
 
 export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "https://managix.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     return app(req, res);
 }
